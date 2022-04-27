2cb0576ef860f46f532f83e4214712b207a45b70
GitHWS <gitcodeHW@gmail.com>
2022-04-28 00:25:50 +0900

    Add TIL file

---
+++ Day19/Javascript/002.js
@@ -0,0 +1,7 @@
+document.getElementById("one").innerHTML =
+  "HELLO <strong>WORLD document</strong>";
+console.log("hello world console");
+window.alert("hello world alert");
+const word = window.prompt("무엇을 입력하시겠습니까?");
+console.log(word);
+document.write(word);
